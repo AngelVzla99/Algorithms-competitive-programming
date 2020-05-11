89e1d487ec5cc5a66c996addaf0311a882a93b75
jesuswr <jesuswahrman@gmail.com>
2020-05-11 17:00:12 -0400

    dsu code

---
+++ dataStructures/DSU.cpp
@@ -0,0 +1,21 @@
+int father[maxN], siz[maxN];
+// n es el primer elemento y m el ultimo, por ejemplo los nodos de un grafo desde el 1 al 10
+void makeSet(int n, int m){
+	for (int i = n; i <= m; ++i){
+		father[i] = -1;
+		siz[i] = 0;
+	}
+}
+int find(int x){
+	if (father[x]==-1) // -1 significa que es el representante del set
+		return x;
+	return father[x] = find(father[x]);
+}
+void unio(int x, int y){
+	if (siz[x] > siz[y]) 
+		father[y] = x;
+	else{
+		father[x] = y;
+		if ( siz[x] == siz[y] ) siz[y]++;
+	}
+}
