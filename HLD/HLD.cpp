28a8500b4be8bd76c96c984b4c80e9502d74fed9
Jesus Wahrman <41008968+jesuswr@users.noreply.github.com>
2020-10-28 22:32:22 -0400

    Update HLD.cpp

--- HLD/HLD.cpp
+++ HLD/HLD.cpp
@@ -19,17 +19,17 @@ void hld(int x, int c){
 	for(int y:G[x])if(y!=mx&&y!=dad[x])hld(y,-1);
 }
 void hld_init(){dad[0]=-1;dep[0]=0;dfs1(0);curpos=0;hld(0,-1);}
-int query(int x, int y, STree& rmq){
+int query(int x, int y){
 	int r=NEUT;
 	while(head[x]!=head[y]){
 		if(dep[head[x]]>dep[head[y]])swap(x,y);
-		r=oper(r,rmq.query(pos[head[y]],pos[y]+1));
+		r=oper(r,STQ(pos[head[y]],pos[y]+1).val);
 		y=dad[head[y]];
 	}
 	if(dep[x]>dep[y])swap(x,y); // now x is lca
-	r=oper(r,rmq.query(pos[x],pos[y]+1));
+	r=oper(r,STQ(pos[x],pos[y]+1).val);
 	return r;
 }
-// for updating: rmq.upd(pos[x],v);
+// for updating: STU(pos[x],u)
 // queries on edges: - assign values of edges to "child" node
 //                   - change pos[x] to pos[x]+1 in query (line 30)
