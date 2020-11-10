605df6eb008491d05e0c52718023c9994273b781
Angel <angelmolina19@yahoo.es>
2020-11-10 15:19:21 -0400

    Convex hull que SI sirve

--- geometry/convexHull.cpp
+++ geometry/convexHull.cpp
@@ -1,17 +1,31 @@
-//stores convex hull of P in S, CCW order
-//left must return >=0 to delete collinear points!
-void CH(vector<pto>& P, vector<pto> &S){
-    S.clear();
-    sort(P.begin(), P.end());//first x, then y
-    FOR(i,0,P.size()){//lower hull
-        while(S.size()>= 2 && S[S.size()-1].left(S[S.size()-2], P[i])) S.pop_back();
-        S.pb(P[i]);
-    }
-    S.pop_back();
-    int k=S.size();
-    ROF(i,P.size()-1,-1){//upper hull
-        while(S.size() >= k+2 && S[S.size()-1].left(S[S.size()-2], P[i])) S.pop_back();
-        S.pb(P[i]);
-    }
-    S.pop_back();
-}
+struct pto { 
+    ld x, y; 
+    bool operator<(pto p){ return x < p.x || (x == p.x && y < p.y);  } 
+}; 
+ 
+ld cross_product(pto O, pto A, pto B){ 
+    return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x); 
+} 
+ 
+// Returns a list of ptos on the convex hull 
+// in counter-clockwise order 
+vector<pto> convex_hull(vector<pto> A) { 
+    int n = A.size(), k = 0; 
+    if(n<=3) return A; 
+    vector<pto> ans(2*n); 
+    sort(A.begin(), A.end()); // Sort ptos lexicographically 
+ 
+    // Build lower hull 
+    for(int i = 0; i < n; ++i){ 
+        while(k>=2 && cross_product(ans[k-2], ans[k-1], A[i])<= 0) k--; 
+        ans[k++] = A[i]; 
+    }  
+    // Build upper hull 
+    for(size_t i = n - 1, t = k + 1; i > 0; --i){ 
+        while(k>=t && cross_product(ans[k-2], ans[k-1], A[i-1])<= 0) k--; 
+        ans[k++] = A[i-1]; 
+    } 
+    // Resize the array to desired size 
+    ans.resize(k-1); 
+    return ans; 
+} 
