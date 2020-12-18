a248d4e1dd04f33748285edd208e5c586034ba60
Angel <angelmolina19@yahoo.es>
2020-12-17 22:34:31 -0400

    Dijkstra

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,25 @@
+vector<ld> dijkstra(int s){
+    vector<ld> dist(n+1,INFLL);
+    bool visit[n+1]={};
+    priority_queue< pair<ld,int>, vector<pair<ld,int>>, greater<pair<ld,int>> > pq;
+
+    dist[s] = 0;
+    pq.push({0,s});
+
+    int v, u;
+    while(!pq.empty()){
+        v = pq.top()._2; pq.pop();
+        if( visit[v] ) continue;
+
+        for(auto e: G[v]){ // G[from].pb({to,cost})
+            u = e.first;
+            ld d = e._2 + dist[v];
+            if( dist[u] > d && !visit[u] ){
+                dist[u] = d;
+                pq.push({d,u});
+            }
+        }
+        visit[v] = true;
+    }
+    return dist;
+}
