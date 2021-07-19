bcd37aace70ccf99d49d11c07653a95e80452045
Angel Garces <angelgarces1248@gmail.com>
2021-07-19 01:58:10 -0400

    LCA con binary lifting

--- graph/LCA.cpp
+++ graph/LCA.cpp
@@ -29,3 +29,34 @@ int lca(int u, int v){
     }
     return anc[u][0];
 }
+
+// ============== Codigo con solo binary lifting
+
+const int K = log2(MAXN)+1;
+vector<int> G[1<<K];int n;  // K such that 2^K>=n
+int F[K][1<<K],D[1<<K];
+void lca_dfs(int x){
+	FOR(i,0,G[x].size()){
+		int y=g[x][i];if(y==F[0][x])continue;
+		F[0][y]=x;D[y]=D[x]+1;lca_dfs(y);
+	}
+}
+void lca_init(){
+	D[0]=0;F[0][0]=-1;
+	lca_dfs(0); // graph indexed in 0
+	FOR(k,1,K) FOR(x,0,n){ 
+		if(F[k-1][x]<0)F[k][x]=-1;
+		else F[k][x]=F[k-1][F[k-1][x]];
+	}
+}
+int lca(int x, int y){
+	if(D[x]<D[y])swap(x,y);
+	for(int k=K-1;k>=0;--k)
+		if(D[x]-(1<<k)>=D[y])
+			x=F[k][x];
+	if(x==y)return x;
+	for(int k=K-1;k>=0;--k)
+		if(F[k][x]!=F[k][y])
+			x=F[k][x],y=F[k][y];
+	return F[0][x];
+}
